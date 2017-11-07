56e8191373615bbbb8e08a5acb40c1215db761e4
Geovani <geovanirog@gmail.com>
2017-11-07 13:40:51 -0200

    video 1

---
+++ module/Application/src/Controller/IndexController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    protected $container;
+
+    public function __construct($container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function indexAction()
+    {
+
+        $sm = $this->container->get('Doctrine\ORM\EntityManager');
+
+        //PDO
+        $conn = $sm->getConnection();
+
+        $res = $conn->prepare('SELECT * FROM tb_books');
+        $res->execute();
+        $red = $res->fetchAll();
+
+        foreach($red as $data){
+          echo $data['name'].' - '.$data['author'];
+          echo '<br>';
+        }
+
+
+        exit;
+
+        return new ViewModel();
+    }
+}
